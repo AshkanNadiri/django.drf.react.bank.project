a82100343fa4fafb4fdd10c058a94489f0a5da68
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-12-23 14:51:04 -0800

    finished with stylying on branchitem

--- src/components/layout/Branchitem.js
+++ src/components/layout/Branchitem.js
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types'
 
 class Branchitem extends Component {
     itemGetStyle = () => {
-        if(this.props.branch.deleted){
-            return {
-                textDecoration : 'line-through'
-            }
-        }else {
-            return{
-                textDecoration: 'none'
-            }
+        // if(this.props.branch.deleted){
+        //     return {
+        //         textDecoration : 'line-through'
+        //     }
+        // }else {
+        //     return{
+        //         textDecoration: 'none'
+        //     }
+        // }
+        return {
+            backgroundColor : "#f4f4f4",
+            padding: '4px',
+            borderBottom: '1px solid #555555',
+            textDecoration: this.props.branch.deleted ? 'line-through' : 'none'
         }
     }
     render() {
