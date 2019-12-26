7e3cc5382025a403b2586da745d25b8fefc94552
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-12-26 13:08:55 -0800

    fixed url tabs on header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,10 +9,10 @@ function Header() {
       <header style={headerStyle}>
             <h1 style= {{color: '#fff'}}> MRM BANK</h1>
             <Link style={linkStyle} to="/">home</Link>
-            <Link style={linkStyle} to="/branch">branches</Link>
-            <Link style={linkStyle} to="/account">accounts</Link>
-            <Link style={linkStyle} to="/customer">customers</Link>   
-            <Link style={linkStyle} to="/product"> products</Link> 
+            <Link style={linkStyle} to="/branches">branches</Link>
+            <Link style={linkStyle} to="/accounts">accounts</Link>
+            <Link style={linkStyle} to="/customers">customers</Link>   
+            <Link style={linkStyle} to="/products"> products</Link> 
       </header>
     )
    
