6532c99c5a96c1c89a961d818fe85fc9b97f5673
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-12-30 15:27:48 -0800

    update

--- frontendbank/src/components/pages/Home.js
+++ frontendbank/src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <home style={homeStyle}>
-            <h3> This is the Home page </h3>
+            <h3> Click on <strong> Branch</strong> to try this App</h3>
             <p> Just you know </p>
         </home>
     )
