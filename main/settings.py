cc7ce7a26c3818ad8495dfe7e5862229844b21d1
AshkanNadiri <alireza_nadiri70@yahoo.com>
2020-01-07 14:13:34 -0800

    fixed bug: fixed a bug on settings.py byt adding  after knox on line 62

--- main/settings.py
+++ main/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES' : ('knox.auth.TokenAuthentication')
+    'DEFAULT_AUTHENTICATION_CLASSES' : ('knox.auth.TokenAuthentication',)
 }
 
 ROOT_URLCONF = 'main.urls'
