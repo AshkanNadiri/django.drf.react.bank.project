c28def8d07ffb373713d4524fc2b0790b4325c41
AshkanNadiri <alireza_nadiri70@yahoo.com>
2020-01-08 13:24:46 -0800

    add: added Register.js to register a user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Home from './components/pages/Home';
 import Branches from './components/pages/Branches';
