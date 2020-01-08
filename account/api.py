fd0f670ae186a2ddbaeb3be1495237bc3ff405f1
AshkanNadiri <alireza_nadiri70@yahoo.com>
2020-01-07 16:18:03 -0800

    add: added generics and permission to the api.py

---
+++ account/api.py
@@ -0,0 +1,21 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Responses
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+  serializer_class = RegisterSerializer
+
+  def post(self, request, *args, **kwargs):
+    serializer = self.get_serializer(data = request.data)
+    serializer.is_valid(raise_exception =True)
+    user = serializer.save()
+    return Response({
+      'user': UserSerializer(user, context = self.get_serializer_context()).data,
+      'token': AuthToken.objects.create(user)
+    })
+
+# Login API
+
+# Get User API
---
+++ account/urls.py
@@ -0,0 +1,3 @@
+from django.urls import path, include
+from .api import RegisterAPI
+from knox import views
