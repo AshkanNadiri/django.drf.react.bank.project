73be4c84cec7cf57d5fbbf25d61f260822671a0e
AshkanNadiri <alireza_nadiri70@yahoo.com>
2020-01-09 11:51:58 -0800

    add: add error.js to the reducer

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,16 +1,12 @@
 import { combineReducers } from 'redux'
 import auth from './auth'
+import customers from '../pages/Customers'
 
 
 
-export default combineReducers({ auth })
+export default combineReducers({ 
+  auth,
+  customers
+ })
 
 
-
-
-// function combineReducers() {
-
-// }
-
-
-// export default 
