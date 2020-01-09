cd5183e2ee2a3ea0d2b884143e6d8454fd2f2e81
AshkanNadiri <geekwise.ashkan.nadiri@gmail.com>
2020-01-09 14:55:45 -0800

    add: added action folder

---
+++ src/components/action/types.js
@@ -0,0 +1,11 @@
+export const GET_CUSTOMERS = 'GET_CUSTOMERS';
+export const DELETE_CUSTOMER = 'DELETE_CUSTOMER';
+export const ADD_CUSTOMER = 'ADD_CUSTOMER';
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
