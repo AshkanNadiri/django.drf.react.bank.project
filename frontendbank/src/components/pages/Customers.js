5ace5b0b5001040a74855095f36a343d352bab8a
AshkanNadiri <geekwise.ashkan.nadiri@gmail.com>
2020-01-10 13:27:06 -0800

    delete

--- frontendbank/src/components/pages/Customers.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Customer() {
-    return (
-        <customer>
-            <h1>This customer page</h1>
-        </customer>
-    )
-}
--- frontendbank/src/components/pages/Products.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Product() {
-    return (
-        <product>
-            <h1>Product</h1>
-        </product>
-    )
-}
