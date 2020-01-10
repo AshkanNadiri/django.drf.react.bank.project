cd276ca32d81d6d13ad6cc95c0e8ebb07844c616
AshkanNadiri <geekwise.ashkan.nadiri@gmail.com>
2020-01-10 14:24:11 -0800

    messages file was added

--- src/components/action/auth.js
+++ src/components/action/auth.js
@@ -1,77 +1,113 @@
 import axios from 'axios';
+import { returnErrors } from './messages'
 import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_FAIL,
     LOGIN_SUCCESS, LOGOUT_SUCCESS, REGISTER_FAIL, REGISTER_SUCCESS } from './types'
 
+// Check the token and load user
 export const loadUser = () => (dispatch, getState) => {
-    dispatch({type: USER_LOADING});
-    axios.get('https://bank-django-drf-local.herokuapp.com/api/auth/user', tokenConfig(getState))
-    .then(res => {
-        dispatch({
-            type: USER_LOADED,
-            payload: res.data,
-        })
-    })
-    .catch(err => {
-        dispatch({type: AUTH_ERROR});
-    })
-}
+    // user loading
+    dispatch({ type: USER_LOADING });
 
-export const login = (username, password) => dispatch => {
+    //get token from state
+    const token = getState().auth.token
+
+    //headers
     const config = {
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type':'application/json'
         }
     }
-    const body = JSON.stringify({username, password});
-    axios.post('https://bank-django-drf-local.herokuapp.com/api/auth/login', body, config)
-    .then(res => {
-        dispatch({
-            type: LOGIN_SUCCESS,
-            payload: res.data,
-        })})
-    .catch(err => {
-        dispatch({type: LOGIN_FAIL});
-    })
-}
 
-export const register = ({username, email, password}) => dispatch => {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }
-    const body = JSON.stringify({username, email, password});
-    axios.post('https://bank-django-drf-local.herokuapp.com/api/auth/register', body, config)
-    .then(res => {
-        dispatch({
-            type: REGISTER_SUCCESS,
-            payload: res.data,
-        })})
-    .catch(err => {
-        dispatch({type: REGISTER_FAIL});
-    })
-}
-
-export const logout = () => (dispatch, getState) => {
-    axios.post('https://bank-django-drf-local.herokuapp.com/api/auth/logout', null, tokenConfig(getState))
-    .then(res => {
-        dispatch({
-            type: LOGOUT_SUCCESS,
-        })
-    })   
-    .catch(err => console.log(err));
-}
-
-export const tokenConfig = getState => {
-    const token = getState().auth.token;
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }
+    // If token, add to headers config
     if(token) {
-        config.headers['Authorization'] = `Token ${token}`
+        config.headers['Authorization'] = `Token ${token}` ;
     }
 
-    return config;
+    axios.get('https://bank-django-drf-local.herokuapp.com/api/auth/user', config)
+        .then(res => {
+            dispatch ({
+                type: USER_LOADED,
+                payload : res.data
+            })
+        }).catch (err => {
+            dispatch(returnErrors(err.response.data, err.response.status))
+        })
 }
+
+
+
+
+
+// export const loadUser = () => (dispatch, getState) => {
+//     dispatch({type: USER_LOADING});
+//     axios.get('https://bank-django-drf-local.herokuapp.com/api/auth/user', tokenConfig(getState))
+//     .then(res => {
+//         dispatch({
+//             type: USER_LOADED,
+//             payload: res.data,
+//         })
+//     })
+//     .catch(err => {
+//         dispatch({type: AUTH_ERROR});
+//     })
+// }
+
+// export const login = (username, password) => dispatch => {
+//     const config = {
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     }
+//     const body = JSON.stringify({username, password});
+//     axios.post('https://bank-django-drf-local.herokuapp.com/api/auth/login', body, config)
+//     .then(res => {
+//         dispatch({
+//             type: LOGIN_SUCCESS,
+//             payload: res.data,
+//         })})
+//     .catch(err => {
+//         dispatch({type: LOGIN_FAIL});
+//     })
+// }
+
+// export const register = ({username, email, password}) => dispatch => {
+//     const config = {
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     }
+//     const body = JSON.stringify({username, email, password});
+//     axios.post('https://bank-django-drf-local.herokuapp.com/api/auth/register', body, config)
+//     .then(res => {
+//         dispatch({
+//             type: REGISTER_SUCCESS,
+//             payload: res.data,
+//         })})
+//     .catch(err => {
+//         dispatch({type: REGISTER_FAIL});
+//     })
+// }
+
+// export const logout = () => (dispatch, getState) => {
+//     axios.post('https://bank-django-drf-local.herokuapp.com/api/auth/logout', null, tokenConfig(getState))
+//     .then(res => {
+//         dispatch({
+//             type: LOGOUT_SUCCESS,
+//         })
+//     })   
+//     .catch(err => console.log(err));
+// }
+
+// export const tokenConfig = getState => {
+//     const token = getState().auth.token;
+//     const config = {
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     }
+//     if(token) {
+//         config.headers['Authorization'] = `Token ${token}`
+//     }
+
+//     return config;
+// }
--- src/components/reducers/auth.js
+++ src/components/reducers/auth.js
@@ -1,3 +1,5 @@
+import { USER_LOADED ,USER_LOADING, AUTH_ERROR} from "../action/types"
+
 const initialState = {
     token: localStorage.getItem('token'),
     isLoading : false ,
@@ -6,8 +8,30 @@ const initialState = {
 }
 
 
-export default (state=initialState) => {
-  return state 
-  // return state
-
+export default (state=initialState, action) => {
+  switch (action.type){
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case USER_LOADED :
+      return {
+        ...state,
+        isAuthenticated : true,
+        isLoading : false,
+        user: action.payload
+      }
+    case AUTH_ERROR:
+      localStorage.removeItem('token')
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false
+      }
+    default:
+      return state;
+  }
 }
